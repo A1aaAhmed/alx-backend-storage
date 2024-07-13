e347258a2c9d032d88f487994c9d532929a9f125
A1aaAhmed <81260750+A1aaAhmed@users.noreply.github.com>
2024-07-13 14:08:15 +0300

    Update

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
--- List all bands with Glam rock as their main style, ranked by their longevity
+-- List all bands with Glam rock as their main style, ranked by their longevity.
 -- Column names are `band_name` and `lifespan` (in years until 2022)
 -- Attributes `formed` and `split` are used for computing the lifespan
 SELECT band_name, (COALESCE(split, 2022) - formed) AS lifespan
